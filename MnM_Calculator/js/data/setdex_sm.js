bdae7e036991ae08d1bc65bf1f3568bb17861e8b
Vhayle <pigeyrecyril@gmail.com>
2017-06-27 18:38:34 +0200

    Add files via upload

---
+++ MnM_Calculator/js/data/setdex_sm.js
@@ -0,0 +1,148 @@
+var SETDEX_SM = {
+	"Kartana Metagrossite": {
+		"Wallbreaker": {"level":100,"evs":{"at":252,"hp":4,"sp":252},"nature":"Jolly","ability":"Tough Claws","item":"Metagrossite","moves":["Leaf Blade","Sacred Sword","Return","Night Slash"]},
+	},
+	"Groudon-Primal": {
+		"Specially Defensive": {"level":100,"evs":{"hp":252,"df":4,"sd":252},"nature":"Sassy","ability":"Desolate Land","item":"Red Orb","moves":["Thunderbolt","Lava Plume","Precipice Blades","Thunder"]},
+		"Double Dance": {"level":100,"evs":{"at":252,"hp":140,"sp":116},"nature":"Adamant","ability":"Desolate Land","item":"Red Orb","moves":["Precipice Blades","Stone Edge","Fire Punch","Dragon Claw"]},
+	},
+	"Blissey Sablenite": {
+		"Specially Defensive": {"level":100,"evs":{"hp":4,"df":252,"sd":252},"nature":"Bold","ability":"Magic Bounce","item":"Sablenite","moves":["Shadow Ball","Ice Beam","Hyper Voice","Flamethrower"]},
+	},
+	"Deoxys-Speed Pidgeotite": {
+		"Zap Cannon Abuser": {"level":100,"evs":{"hp":36,"sa":252,"sp":220},"nature":"Modest","ability":"No Guard","item":"Pidgeotite","moves":["Psycho Boost","Zap Cannon","Focus Blast","Ice Beam"]},
+	},
+	"Weavile Glalitite": {
+		"-Ate Revenge Killer": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Jolly","ability":"Refrigerate","item":"Glalitite","moves":["Fake Out","Feint","Return","Low Kick"]},
+	},
+	"Golisopod Blue Orb": {
+		"Red Orb Check": {"level":100,"evs":{"hp":248,"at":252,"sd":8},"nature":"Adamant","ability":"Primordial Sea","item":"Blue Orb","moves":["First Impression","Liquidation","Aqua Jet","Leech Life"]},
+	},
+	"Zapdos Venusaurite": {
+		"Physically Defensive": {"level":100,"evs":{"hp":248,"df":252,"sd":8},"nature":"Bold","ability":"Thick Fat","item":"Venusaurite","moves":["Volt Switch","Heat Wave","Hidden Power Ice","U-Turn"]},
+	},
+	"Zapdos Sablenite": {
+		"Physically Defensive": {"level":100,"evs":{"hp":248,"df":252,"sd":8},"nature":"Bold","ability":"Magic Bounce","item":"Sablenite","moves":["Volt Switch","Heat Wave","Hidden Power Ice","U-Turn"]},
+	},
+	"Manaphy Absolite": {
+		"Tail Glow Sweeper": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Timid","ability":"Magic Bounce","item":"Absolite","moves":["Surf","Ice Beam","Energy Ball","Psychic"]},
+	},
+	"Zygarde Pinsirite": {
+		"Offensive Dragon Dance": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Return","Extreme Speed","Thousand Arrows","Outrage"]},
+		"Bulky Dragon Dance 101HP Sub": {"level":100,"evs":{"hp":192,"at":252,"df":8,"sp":56},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Return","Extreme Speed","Thousand Arrows","Outrage"]},
+	},
+	"Arceus-Ghost": {
+		"Physically Defensive": {"level":100,"evs":{"hp":252,"df":232,"sp":24},"nature":"Bold","ability":"Multitype","item":"Spooky Plate","moves":["Judgment","Ice Beam"]},
+		"Z-Sweeper": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Jolly","ability":"Multitype","item":"Ghostium Z","moves":["Shadow Force","Earthquake","Brick Break","Extreme Speed"]},
+	},
+	"Terrakion Lucarionite": {
+		"Wallbreaker": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Jolly","ability":"Adaptability","item":"Lucarionite","moves":["Close Combat","Stone Edge","Earthquake","Quick Attack"]},
+	},
+	"Magearna Sablenite": {
+		"Defensive Pivot": {"level":100,"evs":{"hp":252,"sa":252,"df":4},"nature":"Modest","ability":"Magic Bounce","item":"Sablenite","moves":["Fleur Cannon","Flash Cannon","Volt Switch","Aura Sphere"]},
+	},
+	"Magearna Venusaurite": {
+		"Defensive Pivot": {"level":100,"evs":{"hp":252,"sa":252,"df":4},"nature":"Modest","ability":"Thick Fat","item":"Venusaurite","moves":["Fleur Cannon","Flash Cannon","Volt Switch","Aura Sphere"]},
+	},
+	"Tapu Lele": {
+		"Choice Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Psychic Surge","item":"Choice Scarf","moves":["Psychic","Psyshock","Focus Blast","Thunder"]},
+	},
+	"Mimikyu Metagrossite": {
+		"Swords Dance Sweeper": {"level":100,"evs":{"df":4,"at":252,"sp":252},"nature":"Jolly","ability":"Tough Claws","item":"Metagrossite","moves":["Shadow Claw","Shadow Sneak","Play Rough","Wood Hammer"]},
+	},
+	"Xerneas": {
+		"Power Herb Sweeper": {"level":100,"evs":{"hp":212,"sa":252,"sp":44},"nature":"Modest","ability":"Fairy Aura","item":"Power Herb","moves":["Moonblast","Focus Blast","Thunder","Grass Knot"]},
+		"Zeomancy": {"level":100,"evs":{"hp":252,"df":44,"sa":212},"nature":"Modest","ability":"Fairy Aura","item":"Fairium Z","moves":["Moonblast","Focus Blast","Thunder","Grass Knot"]},
+		"Choice Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Fairy Aura","item":"Choice Scarf","moves":["Moonblast","Focus Blast","Thunder","Grass Knot"]},
+	},
+	"Zygarde-Complete": {
+		"Coil / Physically Defensive": {"level":100,"evs":{"hp":252,"df":252,"sd":4},"nature":"Impish","ability":"Power Construct","item":"Leftovers","moves":["Thousand Arrows","Thousand Waves"]},
+	},
+	"Kyogre": {
+		"Choice Scarf": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Timid","ability":"Drizzle","item":"Choice Scarf","moves":["Water Spout","Origin Pulse","Ice Beam","Thunder"]},
+	},
+	"Arceus-Ground": {
+		"Z Sweeper": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Jolly","ability":"Multitype","item":"Groundium Z","moves":["Earthquake","Stone Edge","Extreme Speed"]},
+	},
+	"Cresselia Sablenite": {
+		"Calm Mind": {"level":100,"evs":{"hp":252,"df":252,"sd":4},"nature":"Bold","ability":"Magic Bounce","item":"Sablenite","moves":["Psychic","Psyshock","Moonblast"]},
+	},
+	"Genesect Pinsirite": {
+		"Shift Gear": {"level":100,"evs":{"df":4,"at":252,"sp":252},"nature":"Hasty","ability":"Aerilate","item":"Pinsirite","moves":["Extreme Speed","Return","U-turn","Explosion"]},
+		"Mixed": {"level":100,"evs":{"at":92,"sa":164,"sp":252},"nature":"Hasty","ability":"Aerilate","item":"Pinsirite","moves":["Extreme Speed","U-turn","Techno Blast","Explosion"]},
+	},
+	"Genesect Glalitite": {
+		"Refrigerate": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Hasty","ability":"Refrigerate","item":"Glalitite","moves":["Extreme Speed","Return","Blaze Kick","U-turn"]},
+	},
+	"Hoopa-Unbound Blastoisinite": {
+		"Special Wallbreaker": {"level":100,"evs":{"hp":252,"sa":252,"sd":4},"nature":"Modest","ability":"Mega Launcher","item":"Blastoisinite","moves":["Dark Pulse","Psyshock","Focus Blast","Thunder"]},
+	},
+	"Hoopa-Unbound Lucarionite": {
+		"Physical Wallbreaker": {"level":100,"evs":{"hp":252,"at":252,"df":4},"nature":"Adamant","ability":"Adaptability","item":"Lucarionite","moves":["Hyperspace Fury","Zen Headbutt","Gunk Shot","Focus Punch"]},
+		"Mixed Wallbreaker": {"level":100,"evs":{"at":252,"sa":252,"sp":4},"nature":"Lonely","ability":"Adaptability","item":"Lucarionite","moves":["Hyperspace Fury","Psychic","Thunder","Gunk Shot"]},
+	},
+	"Entei Pinsirite": {
+		"-Ate Attacker": {"level":100,"evs":{"at":252,"df":4,"sp":252},"nature":"Adamant","ability":"Aerilate","item":"Pinsirite","moves":["Extreme Speed","Return","Sacred Fire","Stone Edge"]},
+	},
+	"Keldeo Pidgeotite": {
+		"Calm Mind Sweeper": {"level":100,"evs":{"hp":4,"sa":252,"sp":252},"nature":"Timid","ability":"No Guard","item":"Pidgeotite","moves":["Hydro Pump","Secret Sword","Focus Blast"]},
+	},
+	"Keldeo Lucarionite": {
+		"Calm Mind Sweeper": {"level":100,"evs":{"hp":4,"sa":252,"sp":252},"nature":"Timid","ability":"Adaptability","item":"Lucarionite","moves":["Hydro Pump","Secret Sword","Focus Blast"]},
+		"Swords Dance Sweeper": {"level":100,"evs":{"hp":4,"at":252,"sp":252},"nature":"Jolly","ability":"Adaptability","item":"Lucarionite","moves":["Aqua Jet","Close Combat","Stone Edge","Poison Jab"]},
+	},
+	"Raikou Red Orb": {
+		"Calm Mind Sweeper": {"level":100,"evs":{"df":4,"sa":252,"sp":252},"nature":"Rash","ability":"Desolate Land","item":"Red Orb","moves":["Weather Ball","Thunderbolt","Volt Switch"]},
+	},
+	"Skarmory Blue Orb": {
+		"Physically Defensive": {"level":100,"evs":{"df":252,"hp":252,"sd":4},"nature":"Impish","ability":"Primordial Sea","item":"Blue Orb","moves":["Brave Bird","Iron Head"]},
+	},
+	"Tapu Koko Pidgeotite": {
+		"Calm Mind Sweeper": {"level":100,"evs":{"hp":4,"sa":252,"sp":252},"nature":"Timid","ability":"No Guard","item":"Pidgeotite","moves":["Thunder","Grass Knot","U-turn"]},
+	},
+	"Yveltal": {
+		"Physically Defensive": {"level":100,"evs":{"hp":252,"df":252,"sd":4},"nature":"Impish","ability":"Dark Aura","item":"Leftovers","moves":["Foul Play","U-turn"]},
+		"Life Orb Attacker": {"level":100,"evs":{"sa":252,"sp":252,"at":4},"nature":"Naive","ability":"Dark Aura","item":"Life Orb","moves":["Dark Pulse","Oblivion Wing","Heat Wave","Sucker Punch"]},
+	},
+	"Cobalion Pinsirite": {
+		"Swords Dance Sweeper": {"level":100,"evs":{"at":252,"hp":4,"sp":252},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Return","Quick Attack","Close Combat","Stone Edge"]},
+	},
+	"Gengar Pidgeotite": {
+		"Hypnosis Special Attacker": {"level":100,"evs":{"sa":252,"sp":252,"hp":4},"nature":"Timid","ability":"No Guard","item":"Pidgeotite","moves":["Shadow Ball","Sludge Wave","Focus Blast"]},
+	},
+	"Gengar-Mega": {
+		"Offensive Trapper": {"level":100,"evs":{"sa":252,"sp":252,"hp":4},"nature":"Timid","ability":"Shadow Tag","item":"Gengarite","moves":["Shadow Ball","Sludge Wave","Focus Blast"]},
+	},
+	"Lunala": {
+		"Revenge Killer": {"level":100,"evs":{"sa":252,"sp":252,"hp":4},"nature":"Timid","ability":"Shadow Shield","item":"Choice Scarf","moves":["Moongeist Beam","Moonblast","Focus Blast","Psyshock"]},
+		"Zypnosis + Calm Mind Sweeper": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Modest","ability":"Shadow Shield","item":"Psychium Z","moves":["Moongeist Beam","Moonblast","Focus Blast","Psyshock"]},
+	},
+	"Xurkitree Pidgeotite": {
+		"Tail Glow + Web Sweeper": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Modest","ability":"No Guard","item":"Pidgeotite","moves":["Zap Cannon","Grass Knot"]},
+	},
+	"Volcarona Red Orb": {
+		"Quiver Dance Sweeper": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Timid","ability":"Desolate Land","item":"Red Orb","moves":["Fire Blast","Solar Beam","Hidden Power Rock"]},
+	},
+	"Arcanine Pinsirite": {
+		"Moonlight + 3 Attacks": {"level":100,"evs":{"at":252,"sp":252,"df":4},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Return","Extreme Speed","Flare Blitz","Wild Charge"]},
+		"Physically Defensive": {"level":100,"evs":{"hp":248,"df":252,"sd":8},"nature":"Impish","ability":"Aerilate","item":"Pinsirite","moves":["Extreme Speed","Flamethrower"]},
+	},
+	"Tapu Lele Lucarionite": {
+		"Special Wallbreaker": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Modest","ability":"Adaptability","item":"Lucarionite","moves":["Psychic","Psyshock","Moonblast","Focus Blast"]},
+	},
+	"Tapu Lele Absolite": {
+		"Calm Mind + 3 Attacks": {"level":100,"evs":{"sa":252,"sp":252,"df":4},"nature":"Timid","ability":"Magic Bounce","item":"Absolite","moves":["Psychic","Psyshock","Moonblast","Focus Blast"]},
+	},
+	"Garchomp Lucarionite": {
+		"Swords Dance Sweeper": {"level":100,"evs":{"at":252,"sp":252,"hp":4},"nature":"Jolly","ability":"Adaptability","item":"Lucarionite","moves":["Earthquake","Outrage","Stone Edge"]},
+	},
+	"Kyogre-Primal": {
+		"Physically Defensive": {"level":100,"evs":{"hp":252,"df":252,"sd":4},"nature":"Bold","ability":"Primordial Sea","item":"Blue Orb","moves":["Scald","Ice Beam","Thunder"]},
+	},
+	"Metagross Pinsirite": {
+		"Physical Attacker": {"level":100,"evs":{"at":252,"sp":252,"df":4},"nature":"Jolly","ability":"Aerilate","item":"Pinsirite","moves":["Return","Meteor Mash","Earthquake","Explosion"]},
+	},
+	"Arceus": {
+		"EKiller": {"level":100,"evs":{"at":252,"sp":252,"df":4},"nature":"Jolly","ability":"Multitype","item":"Life Orb","moves":["Extreme Speed","Earthquake","Shadow Claw"]},
+	},
+}
